027d0154cfd07164a63566af4b6b6ddd49d733fd
Shumay <alexeev1one@mail.ru>
2025-05-21 02:07:55 +0500

    init app tasks

---
+++ backend_main/audio_report/tasks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from users.models import Employee
+
+
+class Task(models.Model):
+    content = models.TextField()
+    due_date = models.DateField()
+    assigned_date = models.DateField(auto_now_add=True)
+    employee = models.ForeignKey(Employee, on_delete=models.SET_NULL, blank=True, null=True, related_name='tasks')
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.content[:20]}..."
