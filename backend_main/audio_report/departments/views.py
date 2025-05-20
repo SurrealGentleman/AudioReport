f592840a5cddf0ff68b1d2f81edaf5ab08aee176
Shumay <alexeev1one@mail.ru>
2025-05-21 01:31:59 +0500

    init app departments

---
+++ backend_main/audio_report/departments/views.py
@@ -0,0 +1,29 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Department
+from .serializers import DepartmentSerializer
+
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    http_method_names = ['get', 'post', 'delete']
+
+    def create(self, request, *args, **kwargs):
+        # Создаём
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        # Получаем
+        all_departments = self.get_queryset()
+        response_serializer = self.get_serializer(all_departments, many=True)
+
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        departments = self.get_queryset()
+        serializer = self.get_serializer(departments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
