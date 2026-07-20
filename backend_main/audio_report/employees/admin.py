319040e2c1230ada8486a57a069bcea8a21fb55d
Shumay <alexeev1one@mail.ru>
2026-07-20 07:35:43 +0300

    refactor(backend): remove legacy backend implementation

--- backend_main/audio_report/employees/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Employee
-
-
-admin.site.register(Employee)  # Регистрируем модель в админке
--- backend_main/audio_report/meetings/render_docx.py
+++
@@ -1,6 +0,0 @@
-from docxtpl import DocxTemplate
-
-def render_docx_template(template_path, context, output_path):
-    doc = DocxTemplate(template_path)
-    doc.render(context)
-    doc.save(output_path)
